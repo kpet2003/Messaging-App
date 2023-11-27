70435b7baee9a83d06f1eeee4985f5b02d69f715
kpet2003 <konpet2003@gmail.com>
2023-11-27 09:48:26 +0200

    start breaking down the message

--- ipc_functions.c
+++ ipc_functions.c
@@ -25,28 +25,36 @@ static void print_message(char* memory) {
    return;
 }
 
+
+
 static char* get_message(void) {
-    char* message = malloc(15*sizeof(char));
-    fgets(message,15*sizeof(char),stdin);
+    char* message = malloc(MAX_SIZE*sizeof(char));
+    fgets(message,MAX_SIZE*sizeof(char),stdin);
     return message;
 }
 
-static void write_message(char* memory) {
+static void write_message(Data data) {
     char* message = get_message();
-    memcpy(memory,message,BUFFER_SIZE);
+    data->stats->sent_messages++;
+    data->shared_memory->total_segments = ((strlen(message)+14)/15);
+    data->stats->total_segments_sent+=data->shared_memory->total_segments;
+    memcpy(data->message,message,strlen(message)+1);
     free(message);
 }
 
 
+
+
 void* send_message(void* data) {
     Data my_data = (Data)data;
-    Memory memory = my_data->shared_memory;
     while(true) {
-        write_message(memory->buffer);
-        my_data->stats->sent_messages++;
-        sem_post(&memory->writer_sem);
-        if(!strncmp(END_MESSAGE,memory->buffer,5)) {
-            my_data->shared_memory = memory;
+        if(my_data->shared_memory->segments_sent==0) {
+            write_message(my_data);
+        }
+        
+       
+        sem_post(&my_data->shared_memory->writer_sem);
+        if(!strncmp(END_MESSAGE,my_data->message,5)) {
             break;
         }
     }
@@ -55,14 +63,19 @@ void* send_message(void* data) {
 
 void* receive_message(void* data) {
     Data my_data = (Data)data;
-    Memory memory = my_data->shared_memory;
+
     while(true) {
+        sem_wait(&my_data->shared_memory->writer_sem);
         
-        sem_wait(&memory->writer_sem);
-        print_message(memory->buffer);
-        my_data->stats->received_messages++;
-        if(!strncmp(END_MESSAGE,memory->buffer,5)) {
-            my_data->shared_memory = memory;
+        if(my_data->shared_memory->message_sent) {
+            print_message(my_data->message);      
+            my_data->stats->received_messages++;
+            my_data->shared_memory->segments_sent = 0;
+            my_data->shared_memory->message_sent = false;
+        }
+
+        
+        if(!strncmp(END_MESSAGE,my_data->shared_memory->buffer,5)) {
             break;
         }
  
@@ -73,6 +86,5 @@ void* receive_message(void* data) {
 Stats calculate_stats(Stats s) {
     return s;
 }
-
 
 
